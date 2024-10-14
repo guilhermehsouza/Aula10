2aa5c42877eb319b284c15cb991bcf816aa45825
Guilherme Henrique de Souza <guihenry@gmail.com>
2024-10-14 20:00:41 -0300

    14/10

---
+++ escola/src/Paginas/Erro.jsx
@@ -0,0 +1,8 @@
+export default function Erro()
+{
+    return (
+        <div>
+            <h1>Página não Encontrada</h1>
+        </div>
+    );
+}
---
+++ escola/src/Paginas/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <div>
+
+            <h1>Aula 10 - Projeto Escola com MySQL</h1>
+
+                       
+        </div>
+    );
+}
