a2c3ea87873e63ff5b3c319a9d91551561f996d8
Guilherme Henrique de Souza <guihenry@gmail.com>
2024-11-08 09:10:13 -0300

    07/11

--- servidor/index.js
+++ servidor/index.js
@@ -11,7 +11,7 @@ const db = mysql.createConnection({
     host: 'localhost',  
     port: 3306, 
     user: 'root', 
-    password: 'mysqlfatec', 
+    password: '', 
     database: 'aulabd' 
 });
 
